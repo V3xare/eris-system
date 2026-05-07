211b07a0d2f2380aa151ce7f1e8de6fc7156f6a7
Vexare <calledhypocrisy@gmail.com>
2026-05-07 15:22:00 +0300

    new sass package

--- webpack.config.js
+++ webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: [ "style-loader", "css-loader", "sass-loader" ]
+				use: [ "style-loader", "css-loader", { loader: "sass-loader", options: { api: "modern", sassOptions: {} } } ]
 			}
 		]
 	},

e0e8b055e530a17ed4972eb04acde2885de067dc
Vexare <calledhypocrisy@gmail.com>
2026-05-27 23:28:42 +0300

    !

--- source/components/Search/search.tsx
+++ source/components/Search/search.tsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
 import { Button, LangContext, Column, Row, Props, Select, AutoComplete, Input, LangContextType } from "v-eris";
+import { BuildContextType, BuildContext } from "../../components/Build/build";
+import { SettingsInitType } from "../../utility/use.settings";
 
 import "./search.scss"
 
@@ -12,9 +14,13 @@ export const Search = ( props: {
 	const placeholder = props.placeholder ? props.placeholder : lang.get( "Search::input" );
 	const expanded = !!props.active;
 	const tools = props.tools ? props.tools : [];
+	const build: BuildContextType = useContext( BuildContext );
+	let settings: SettingsInitType = build.settings;
+
+	const hidden = props.hidden === undefined ? !settings.getSecureValue( "General::SideBar::SearchBarEnabled" ) : props.hidden;
 
 	return (
-		<div className={ Props.className( "eris-search", { overlay: expanded, hidden: props.hidden, reduced: !expanded }) }>
+		<div className={ Props.className( "eris-search", { overlay: props.active && !props.hidden, hidden: hidden, reduced: !expanded }) }>
 			<AutoComplete 
 				larger 
 				onSelect={ props.onSelect } 
